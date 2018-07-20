0e654885043874f5e5186023b5e65df01fcfa942
cooper warner <coopman0057@gmail.com.com>
2018-07-20 14:44:46 -0500

    Initial commit

---
+++ MEDIEVAL_ADVENTURE.py
@@ -0,0 +1,55 @@
+import time
+
+
+print('hi')
+x=input('whats your name? ')
+print('hi',x)
+x=input('list your family ')
+family=x.split()
+print(family)
+x=input('Do you like cookies? ')
+if x== 'yes':
+    print ('me too! ')
+    
+    x=input ('try my cookie ')
+if x== 'yes':
+        print ('the cookie was poisoned *laughs*')
+        print ('you died')
+        
+    
+if x== 'no':
+    print ('well that is dumb ')
+    time.sleep(3)
+    print ('ill be going now ')
+    time.sleep(3)
+
+        
+print (' you walk down a path and there are 3 roads on one of them there is a band of robbers the 2nd has some thin that lloks like the smoke of a village and the last one has a cave that has one exit to your knowlage. ')
+x=input(' go down 1,2 or 3 ')
+if x== '1':
+            print ('as you walk to the robbers they one by one start to see you ')
+            time.sleep(3)
+           
+            print('as you go past the robbers stand frozen in fear when you turn around there is a ogre!')
+            time.sleep(3)
+            
+            print ('it yells,"what are you doing in my swamp!"')
+            time.sleep(3)
+            print ('YOU DIED')
+            
+                  
+if x== '2':
+            print ('you start to see the roofs of a village but when you get there it is on fire and no one is there. ')
+            time.sleep(3)
+            print ('you walk passed the village and you here yells you peek thrught the bushes and see the bandits running from a huge ogre')
+            print ('you run to the cave and in the cave is gold! you leave and see natives. you trade the some gold for you to go free you leave. you become 
+            
+if x== '3': print ('the cave was a trap when you step in you here yelling. you turn to see natives just a two darts enter your neck and you start to faint.YOU DIED ')
+                   
+           
+    
+   
+    
+    
+    
+    
